e093e3e049e2aa7d870526e9c5fb1e05be3362b2
Chad McAdams <mcadamschad@outlook.com>
2019-12-21 16:55:08 -0800

    Basic sort template
    added first part of number sort with template so can use any type of numbers (int, double, etc)

--- namespaces/cjmMath.cpp
+++ namespaces/cjmMath.cpp
@@ -3,64 +3,75 @@
 
 namespace cjmMath
 {
-    bool isPrime(int n)
+bool isPrime(int n)
+{
+    if (n == 2)
+        return true;
+    if (n == 3)
+        return true;
+    if (n % 2 == 0)
+        return false;
+    if (n % 3 == 0)
+        return false;
+
+    int i = 5, w = 2;
+
+    while (i * i <= n)
     {
-        if(n == 2) return true;
-        if(n == 3) return true;
-        if(n%2 == 0) return false;
-        if(n % 3 == 0) return false;
-
-        int i = 5, w = 2;
-
-        while(i*i <= n)
+        if (n % i == 0)
         {
-            if(n%i == 0)
-            {
-                return false;
-            }
+            return false;
+        }
         i += w;
         w = 6 - w;
-        }
-        return true;
     }
+    return true;
+}
 
-    std::vector<int> primeNums(int i, int j)
+std::vector<int> primeNums(int i, int j)
+{
+    std::vector<int> v;
+    for (i; i <= j; i++)
     {
-        std::vector<int>v;
-        for(i; i<=j; i++)
+        if (isPrime(i))
         {
-            if(isPrime(i))
-            {
-                v.push_back(i);
-            }
+            v.push_back(i);
         }
     }
+}
 
-    std::vector<int> primeFactors(int num)
+std::vector<int> primeFactors(int num)
+{
+    std::vector<int> primeFactors;
+    for (int i = 2; i < num; i++)
     {
-	    std::vector<int> primeFactors;
-	    for(int i=2; i<num; i++)
-	    {
-		    while(num%i==0)
-		    {
-			    num /= i;
-			    primeFactors.push_back(i);
-		    }
-	    }
-	    return primeFactors;
+        while (num % i == 0)
+        {
+            num /= i;
+            primeFactors.push_back(i);
+        }
     }
+    return primeFactors;
+}
 
-    void printVector(std::vector<int>v)
+void printVector(std::vector<int> v)
+{
+    for (int i = 0; i < v.size(); i++)
     {
-	    for(int i=0; i<v.size(); i++)
-	    {
-		    std::cout << v[i] << " ";
-	    }
+        std::cout << v[i] << " ";
     }
+}
 
-    double getMod(double a, double b)
-    {
-        return (a-(b*((int)a/(int)b)));
-    }
+double getMod(double a, double b)
+{
+    return (a - (b * ((int)a / (int)b)));
+}
 
-} // cjmMath
+template <class num> //put before function or class, can use to use wild card class
+std::vector<num> chadSort()
+{
+    std::vector<num> nums;
+    return nums;
+}
+
+} // namespace cjmMath
